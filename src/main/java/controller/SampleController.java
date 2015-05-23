090a770e400b91c7e48643951c51bf87fc9d1060
Blake Su <blake.su@morningstar.com>
2015-05-23 10:51:41 +0800

    add logback

--- src/main/java/controller/SampleController.java
+++ src/main/java/controller/SampleController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import dbmodel.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,12 @@ import java.util.List;
 @RestController
 public class SampleController {
 
+    final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
     @RequestMapping("/users")
     public List<User> users() {
+
+        logger.info("call /users");
         List<User> lstPersons = new ArrayList<User>();
 
         User person = new User();
